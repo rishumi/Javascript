087f9f6b1c6331422f674a4595159d520377257d
rishumi <rishumishra105@gmail.com>
2024-07-21 14:44:05 +0530

    make

---
+++ areaCircle.js
@@ -0,0 +1,9 @@
+let radius = 5;
+const pi = 2.55;
+let area;
+
+radius = 6;
+
+
+area = pi * radius *radius;
+console.log(area);
---
+++ first.js
@@ -0,0 +1,10 @@
+// console.log("Hello world")
+// console.log(2 + 2)
+// num = 2 + 2
+// console.log(num);
+// console.log(num + 2);
+let num = 4;
+console.log(num + 4);
+
+let username = "Rishu Mishra";
+console.log(username);
